029ec7b8898f28c87d914398cb7228e6976e9f41
unknown <rogersbernat@me.com>
2019-08-21 17:15:33 +0200

    removed .history folder

--- .history/terraform/provider_20190820185428.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region     = "eu-west-1"
-  access_key = "AKIAWB4WGC2FVTYQDJ6V"
-  secret_key = "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"
-}
--- .history/terraform/provider_20190821171419.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region     = "eu-west-1"
-  //please don't use this account (is binded to my credit card :() other options didn't worked in my windows machine...
-  access_key = "AKIAWB4WGC2FVTYQDJ6V"
-  secret_key = "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"
-}
