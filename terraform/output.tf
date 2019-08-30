7d85eb66f519fc508e3c0d039ac8c8ef81819d59
rogersbernat <rogersbernat@me.com>
2019-08-30 15:32:18 +0200

    first commit

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance-ip" {
-value = ["${aws_instance.instance-to-test.public_ip}"]
-}
-output "dnsmasq-server-public-ip" {
-value = ["${aws_instance.dnsmasq-server.public_ip}"]
-}
-output "dnsmasq-server-private-ip" {
-value = ["${aws_instance.dnsmasq-server.private_ip}"]
-}
