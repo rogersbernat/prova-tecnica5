671a0ff9e51df20ca0d4b7a9fc3d1bfd7297178f
rogersbernat <rogersbernat@me.com>
2019-09-08 21:38:31 +0200

    leon noel une vie simple // to generate dynamic configuration per subnet the server is deployed

---
+++ dns_resolver/dnsmasq/virtual machine/scripts/dns-ip.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+## The idea is to pass dinamically the DNS server settings knowing the gateway assigned by default from the DHCP server
+##we collect the gateway assigned by the DHCP
+GATEWAY=$(netstat -rn|grep '0.0.0.0'|head -1|awk '{print $2}')
+
+##we split the base addr (remove the last octet and add the default AWS DNS server in the VPN that is base+2)
+BASEADDR="$(echo $GATEWAY | cut -d. -f1-3)"
+DNSSERVER="$(echo $BASEADDR.2)"
+
+##Then verify that we have this settings:
+echo "gateway $GATEWAY"
+echo "dns-server $DNSSERVER"
--- dns_resolver/dnsmasq/virtual machine/scripts/script.sh
+++ dns_resolver/dnsmasq/virtual machine/scripts/script.sh
@@ -4,14 +4,9 @@
 
 sudo apt update && sudo apt install -y dnsmasq
 
-sudo groupadd -r dnsmasq
-sudo useradd -r -g dnsmasq dnsmasq
-
 sudo mv /home/ubuntu/dnsmasq.conf  /etc/dnsmasq.d/dnsmasq.conf
-sudo mv /home/ubuntu/resolv.dnsmasq  /etc/resolv.dnsmasq
 sudo mv /home/ubuntu/dhclient.conf /etc/dhcp/dhclient.conf
 sudo chown dnsmasq: /etc/dnsmasq.d/dnsmasq.conf
-sudo chown dnsmasq: /etc/resolv.dnsmasq
 sudo chown root: /etc/dhcp/dhclient.conf
 
 sudo service dnsmasq start
