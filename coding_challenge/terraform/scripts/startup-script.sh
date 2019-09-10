62697904244cadb4074c0b5ae9e8b2d8b231cf02
rogersbernat <rogersbernat@me.com>
2019-09-10 20:49:00 +0200

    so here with SED I configure the dnsmasq config with the baseaddress.2 I collect with netstat and cut

--- coding_challenge/terraform/scripts/startup-script.sh
+++ coding_challenge/terraform/scripts/startup-script.sh
@@ -4,6 +4,8 @@
 ##we collect the gateway assigned by the DHCP
 GATEWAY=$(netstat -rn|grep '0.0.0.0'|head -1|awk '{print $2}')
 
+echo $GATEWAY > /home/ubuntu/gateway.txt
+
 ##we split the base addr (remove the last octet and add the default AWS DNS server in the VPN that is base+2)
 BASEADDR="$(echo $GATEWAY | cut -d. -f1-3)"
 DNSSERVER="$(echo $BASEADDR.2)"
@@ -11,3 +13,10 @@ DNSSERVER="$(echo $BASEADDR.2)"
 ##Then verify that we have this settings:
 echo "gateway $GATEWAY"
 echo "dns-server $DNSSERVER"
+## replacing configuration
+sudo sed -i -e "s/#dns-server#/${DNSSERVER}/g" /etc/dnsmasq.d/dnsmasq.conf
+sudo sed -i -e "s/#dns-server#/${DNSSERVER}/g" /etc/dhcp/dhclient.conf
+
+sudo systemctl enable dnsmasq
+
+sudo service dnsmasq start
