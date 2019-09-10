66d2b8cbb9d29fbd7a56fb868cbb2e8226e88585
rogersbernat <rogersbernat@me.com>
2019-09-10 20:49:54 +0200

    definitly need to do this

--- dns_resolver/dnsmasq/virtual machine/scripts/script.sh
+++ dns_resolver/dnsmasq/virtual machine/scripts/script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
--- updating repo --
-
 sudo apt update && sudo apt install -y dnsmasq
+
+sudo groupadd -r dnsmasq
+sudo useradd -r -g dnsmasq dnsmasq
 
 sudo mv /home/ubuntu/dnsmasq.conf  /etc/dnsmasq.d/dnsmasq.conf
 sudo mv /home/ubuntu/dhclient.conf /etc/dhcp/dhclient.conf
+
 sudo chown dnsmasq: /etc/dnsmasq.d/dnsmasq.conf
 sudo chown root: /etc/dhcp/dhclient.conf
-
-sudo service dnsmasq start
-
-sudo chkconfig dnsmasq on
 
 dig aws.amazon.com @127.0.0.1
 
 ping -c5 amazon.com
+
+sudo service dnsmasq stop
+sudo systemctl disable dnsmasq
