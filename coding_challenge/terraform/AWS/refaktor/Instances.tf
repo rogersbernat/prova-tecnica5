0bfdbd306332c92c84098592b41b8352b5def4ab
rogersbernat <rogersbernat@me.com>
2019-09-19 22:00:40 +0200

    first comit with basic example of ec2 instance from module

---
+++ coding_challenge/terraform/AWS/refaktor/Instances.tf
@@ -0,0 +1,36 @@
+module "instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  instance_count = 1
+
+  name          = "example-normal"
+  ami           = ${var.ami} # to do
+  instance_type = "c5.large"
+  subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
+  //  private_ips                 = ["172.31.32.5", "172.31.46.20"]
+  vpc_security_group_ids      = [module.security_group.this_security_group_id]
+  associate_public_ip_address = true
+  placement_group             = aws_placement_group.web.id
+
+  root_block_device = [
+    {
+      volume_type = "gp2"
+      volume_size = 10
+    },
+  ]
+
+  ebs_block_device = [
+    {
+      device_name = "/dev/sdf"
+      volume_type = "gp2"
+      volume_size = 5
+      encrypted   = true
+      kms_key_id  = aws_kms_key.this.arn
+    }
+  ]
+
+  tags = {
+    "Env"      = "Private"
+    "Location" = "Secret"
+  }
+}
